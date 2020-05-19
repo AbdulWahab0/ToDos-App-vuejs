2041dae7cb78e8083e5ca080ad5b994f455c8acd
Abdul Wahab <abdulwahab@Abduls-MBP-311.fritz.box>
2020-05-19 13:20:53 +0200

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="ani-slideInDown text-center">
+    <h2 class="fs-h2">Abdul Wahab</h2>
+
+    <p>
+      <a
+        href="https://github.com/d2phap/vuejs-todo"
+        rel="noopener noreferer"
+      >https://github.com/AbdulWahab0</a>
+    </p>
+  </div>
+</template>
